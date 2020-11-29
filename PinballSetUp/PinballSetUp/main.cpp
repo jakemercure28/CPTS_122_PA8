a3d3899033cd9457dc21e02d65adf8140f84e975
maydaym3 <jamestschirhart@gmail.com>
2020-11-29 00:39:40 -0800

    changed window poll event to switch case block statement designing new flipper window is not resizable and AA is enabled now

--- PinballSetUp/PinballSetUp/main.cpp
+++ PinballSetUp/PinballSetUp/main.cpp
@@ -1,96 +1,127 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(700, 900), "Pinball SetUp");
-    sf::CircleShape ball(30.f);
-    ball.setPosition(625.f, 770.f);
-    ball.setFillColor(sf::Color::Blue);
+	sf::ContextSettings settings;
+	// Anti Aliasing Enabled
+	settings.antialiasingLevel = 8;
 
-    //I've added to test
-    sf::CircleShape triangle(30.f, 3);
-    triangle.setPosition(300.f, 200.f);
-    triangle.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(700, 900), "Pinball SetUp", sf::Style::Close | sf::Style::Titlebar,settings);
+
+	// Pinball
+	sf::CircleShape ball(30.f);
+	ball.setPosition(625.f, 770.f);
+	ball.setFillColor(sf::Color::Blue);
+	
+	//I've added to test
+	sf::CircleShape triangle(30.f, 3);
+	triangle.setPosition(300.f, 200.f);
+	triangle.setFillColor(sf::Color::Green);
 
 
-    sf::RectangleShape rectangle(sf::Vector2f(10.f, 750.f));
-    rectangle.setPosition(600.f, 175.f);
-    rectangle.setFillColor(sf::Color::White);
+	sf::RectangleShape rectangle(sf::Vector2f(10.f, 750.f));
+	rectangle.setFillColor(sf::Color::White);
+	rectangle.move(600, 175);
+	//rectangle.setPosition(600.f, 175.f);
 
-    sf::RectangleShape left_rectangle(sf::Vector2f(10.f, 715.f));
-    left_rectangle.setPosition(80.f, 0.f);
-    left_rectangle.setFillColor(sf::Color::White);
+	sf::RectangleShape left_rectangle(sf::Vector2f(10.f, 715.f));
+	left_rectangle.move(80, 0);
+	//left_rectangle.setPosition(80.f, 0.f);
+	left_rectangle.setFillColor(sf::Color::White);
 
-    //to launch the ball, we need to make this move vertically with options for launch strength
-    //that correlate with ball speed
-    sf::ConvexShape launcher;
-    launcher.setPointCount(8);
-    launcher.setPoint(0, sf::Vector2f(625.f, 830.f));
-    launcher.setPoint(1, sf::Vector2f(685.f, 830.f));
-    launcher.setPoint(2, sf::Vector2f(685.f, 845.f));
-    launcher.setPoint(3, sf::Vector2f(665.f, 845.f));
-    launcher.setPoint(4, sf::Vector2f(665.f, 900.f));
-    launcher.setPoint(5, sf::Vector2f(645.f, 900.f));
-    launcher.setPoint(6, sf::Vector2f(645.f, 845.f));
-    launcher.setPoint(7, sf::Vector2f(625.f, 845.f));
-    launcher.setFillColor(sf::Color::Red);
+	//to launch the ball, we need to make this move vertically with options for launch strength
+	//that correlate with ball speed
 
-    sf::ConvexShape corner;
-    corner.setPointCount(3);
-    corner.setPoint(0, sf::Vector2f(700.f, 0.f));
-    corner.setPoint(1, sf::Vector2f(700.f, 60.f));
-    corner.setPoint(2, sf::Vector2f(640.f, 0.f));
+	sf::ConvexShape launcher;
+	launcher.setPointCount(8);
+	launcher.setPoint(0, sf::Vector2f(625.f, 830.f));
+	launcher.setPoint(1, sf::Vector2f(685.f, 830.f));
+	launcher.setPoint(2, sf::Vector2f(685.f, 845.f));
+	launcher.setPoint(3, sf::Vector2f(665.f, 845.f));
+	launcher.setPoint(4, sf::Vector2f(665.f, 900.f));
+	launcher.setPoint(5, sf::Vector2f(645.f, 900.f));
+	launcher.setPoint(6, sf::Vector2f(645.f, 845.f));
+	launcher.setPoint(7, sf::Vector2f(625.f, 845.f));
+	launcher.setFillColor(sf::Color::Red);
 
-    sf::ConvexShape left_corner;
-    left_corner.setPointCount(3);
-    left_corner.setPoint(0, sf::Vector2f(90.f, 0.f));
-    left_corner.setPoint(1, sf::Vector2f(90.f, 60.f));
-    left_corner.setPoint(2, sf::Vector2f(150.f, 0.f));
+	sf::ConvexShape corner;
+	corner.setPointCount(3);
+	corner.setPoint(0, sf::Vector2f(700.f, 0.f));
+	corner.setPoint(1, sf::Vector2f(700.f, 60.f));
+	corner.setPoint(2, sf::Vector2f(640.f, 0.f));
 
-    sf::ConvexShape left_flipper;
-    left_flipper.setPointCount(3);
-    left_flipper.setPoint(0, sf::Vector2f(190.f, 800.f));
-    left_flipper.setPoint(1, sf::Vector2f(305.f, 830.f));
-    left_flipper.setPoint(2, sf::Vector2f(190.f, 830.f));
+	sf::ConvexShape left_corner;
+	left_corner.setPointCount(3);
+	left_corner.setPoint(0, sf::Vector2f(90.f, 0.f));
+	left_corner.setPoint(1, sf::Vector2f(90.f, 60.f));
+	left_corner.setPoint(2, sf::Vector2f(150.f, 0.f));
 
-    sf::ConvexShape right_flipper;
-    right_flipper.setPointCount(3);
-    right_flipper.setPoint(0, sf::Vector2f(500.f, 800.f));
-    right_flipper.setPoint(1, sf::Vector2f(500.f, 830.f));
-    right_flipper.setPoint(2, sf::Vector2f(385.f, 830.f));
+	// new left flipper
+	sf::CircleShape leftflipperBase;
+	leftflipperBase.setRadius(15);
+	leftflipperBase.setPosition(175.f, 800.f);
 
-    sf::RectangleShape left_bound(sf::Vector2f(160.f, 15.f));
-    left_bound.setPosition(90.f, 700.f);
-    left_bound.rotate(45.f);
+	sf::ConvexShape leftflipperArm;
+	leftflipperArm.setPointCount(4);
+	leftflipperArm.setPoint(0, sf::Vector2f(190.f, 800.f));
+	leftflipperArm.setPoint(1, sf::Vector2f(305.f, 805.f));
+	leftflipperArm.setPoint(2, sf::Vector2f(305.f, 825.f));
+	leftflipperArm.setPoint(3, sf::Vector2f(190.f, 830.f));
+	sf::CircleShape leftflipperTip;
+	leftflipperTip.setRadius(10);
+	leftflipperTip.setPosition(295, 805);
 
-    sf::RectangleShape right_bound(sf::Vector2f(160.f, 15.f));
-    right_bound.setPosition(610.f, 710.f);
-    right_bound.rotate(135.f);
+	// old left flipper
+	sf::ConvexShape left_flipper;
+	left_flipper.setPointCount(3);
+	left_flipper.setPoint(0, sf::Vector2f(190.f, 800.f));
+	left_flipper.setPoint(1, sf::Vector2f(305.f, 830.f));
+	left_flipper.setPoint(2, sf::Vector2f(190.f, 830.f));
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	sf::ConvexShape right_flipper;
+	right_flipper.setPointCount(3);
+	right_flipper.setPoint(0, sf::Vector2f(500.f, 800.f));
+	right_flipper.setPoint(1, sf::Vector2f(500.f, 830.f));
+	right_flipper.setPoint(2, sf::Vector2f(385.f, 830.f));
 
-        window.clear();
-        window.draw(ball);
-        window.draw(triangle);
-        window.draw(rectangle);
-        window.draw(left_rectangle);
-        window.draw(launcher);
-        window.draw(corner);
-        window.draw(left_corner);
-        window.draw(left_flipper);
-        window.draw(left_bound);
-        window.draw(right_flipper);
-        window.draw(right_bound);
-        window.display();
-    }
+	sf::RectangleShape left_bound(sf::Vector2f(160.f, 15.f));
+	left_bound.setPosition(90.f, 700.f);
+	left_bound.rotate(45.f);
 
-    return 0;
+	sf::RectangleShape right_bound(sf::Vector2f(160.f, 15.f));
+	right_bound.setPosition(610.f, 710.f);
+	right_bound.rotate(135.f);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+                case sf::Event::Closed:
+					window.close();
+					break;
+
+		}
+
+		window.clear();
+		window.draw(ball);
+		window.draw(triangle);
+		window.draw(rectangle);
+		window.draw(left_rectangle);
+		window.draw(launcher);
+		window.draw(corner);
+		window.draw(left_corner);
+		window.draw(left_flipper);
+		//window.draw(leftflipperBase);
+		//window.draw(leftflipperArm);
+		//window.draw(leftflipperTip);
+		window.draw(left_bound);
+		window.draw(right_flipper);
+		window.draw(right_bound);
+		window.display();
+	}
+
+	return 0;
 }
