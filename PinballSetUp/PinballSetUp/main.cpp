34dffc1d788d0eaba0352bde4001e0203e3341ba
Subashree Venkatasubramanian <s.venkatasubramani2@wsu.edu>
2020-12-02 02:30:55 -0800

    - resized window to 900 by 900 - added 'PINBALL', 'Score:' and score box - added a font file (.ttf) which contains the displayed font

--- PinballSetUp/PinballSetUp/main.cpp
+++ PinballSetUp/PinballSetUp/main.cpp
@@ -10,25 +10,25 @@ int main()
 	// Anti Aliasing Enabled
 	settings.antialiasingLevel = 8;
 
-	sf::RenderWindow window(sf::VideoMode(700, 900), "Pinball SetUp", sf::Style::Close | sf::Style::Titlebar,settings);
+	sf::RenderWindow window(sf::VideoMode(900, 900), "Pinball SetUp", sf::Style::Close | sf::Style::Titlebar,settings);
 
 	// Pinball
-	ball* pinball = new ball(625,770);
+	ball* pinball = new ball(825,770);
 
 	//Flippers
-	flipper* leftFlipper = new flipper(190, 815, 90, false);
-	flipper* rightFlipper = new flipper(500, 815, 90, true);
+	flipper* leftFlipper = new flipper(390, 815, 90, false);
+	flipper* rightFlipper = new flipper(700, 815, 90, true);
 
 	//launcher
-	launcher* ballLauncher = new launcher(655, 830);
+	launcher* ballLauncher = new launcher(855, 830);
 
 	sf::RectangleShape rectangle(sf::Vector2f(10.f, 750.f));
 	rectangle.setFillColor(sf::Color::White);
-	rectangle.move(600, 175);
+	rectangle.move(800, 175);
 	//rectangle.setPosition(600.f, 175.f);
 
 	sf::RectangleShape left_rectangle(sf::Vector2f(10.f, 715.f));
-	left_rectangle.move(80, 0);
+	left_rectangle.move(280, 0);
 	//left_rectangle.setPosition(80.f, 0.f);
 	left_rectangle.setFillColor(sf::Color::White);
 
@@ -38,23 +38,65 @@ int main()
 
 	sf::ConvexShape corner;
 	corner.setPointCount(3);
-	corner.setPoint(0, sf::Vector2f(700.f, 0.f));
-	corner.setPoint(1, sf::Vector2f(700.f, 60.f));
-	corner.setPoint(2, sf::Vector2f(640.f, 0.f));
+	corner.setPoint(0, sf::Vector2f(900.f, 0.f));
+	corner.setPoint(1, sf::Vector2f(900.f, 60.f));
+	corner.setPoint(2, sf::Vector2f(840.f, 0.f));
 
 	sf::ConvexShape left_corner;
 	left_corner.setPointCount(3);
-	left_corner.setPoint(0, sf::Vector2f(90.f, 0.f));
-	left_corner.setPoint(1, sf::Vector2f(90.f, 60.f));
-	left_corner.setPoint(2, sf::Vector2f(150.f, 0.f));
+	left_corner.setPoint(0, sf::Vector2f(290.f, 0.f));
+	left_corner.setPoint(1, sf::Vector2f(290.f, 60.f));
+	left_corner.setPoint(2, sf::Vector2f(350.f, 0.f));
 
 	sf::RectangleShape left_bound(sf::Vector2f(160.f, 15.f));
-	left_bound.setPosition(90.f, 700.f);
+	left_bound.setPosition(290.f, 700.f);
 	left_bound.rotate(45.f);
 
 	sf::RectangleShape right_bound(sf::Vector2f(160.f, 15.f));
-	right_bound.setPosition(610.f, 710.f);
+	right_bound.setPosition(810.f, 710.f);
 	right_bound.rotate(135.f);
+
+	//adding in the text
+	sf::Font MyFont;
+	if (!MyFont.loadFromFile("OCR-A BT.ttf"))
+	{
+		// error...
+	}
+
+	sf::Text title; Text titleshadow; Text score; Text scoreshadow;
+	//title 
+	title.setFont(MyFont);
+	title.setString("PINBALL");
+	title.setCharacterSize(70); 
+	title.setFillColor(sf::Color::Red);
+	title.setPosition(25.f, 750.f);
+	//shadow of title
+	titleshadow.setFont(MyFont);
+	titleshadow.setString("PINBALL");
+	titleshadow.setCharacterSize(70);
+	titleshadow.setFillColor(sf::Color::Blue);
+	titleshadow.setPosition(30.f, 755.f);
+	//score
+	score.setFont(MyFont);
+	score.setString("Score:");
+	score.setCharacterSize(50);
+	score.setFillColor(sf::Color::Red);
+	score.setPosition(45.f, 50.f);
+	//shadow of score
+	scoreshadow.setFont(MyFont);
+	scoreshadow.setString("Score:");
+	scoreshadow.setCharacterSize(50);
+	scoreshadow.setFillColor(sf::Color::Blue);
+	scoreshadow.setPosition(50.f, 55.f);
+
+	sf::RectangleShape score_box1(sf::Vector2f(170.f, 60.f));
+	score_box1.setPosition(55.f, 115.f);
+	score_box1.setFillColor(sf::Color::Red);
+
+	sf::RectangleShape score_box2(sf::Vector2f(170.f, 60.f));
+	score_box2.setPosition(60.f, 120.f);
+	score_box2.setFillColor(sf::Color::Blue);
+
 
 	while (window.isOpen())
 	{
@@ -107,6 +149,12 @@ int main()
 		window.draw(left_corner);
 		window.draw(left_bound);
 		window.draw(right_bound);
+		window.draw(titleshadow);
+		window.draw(scoreshadow);
+		window.draw(title);
+		window.draw(score);
+		window.draw(score_box2);
+		window.draw(score_box1);
 		window.display();
 	}
 
