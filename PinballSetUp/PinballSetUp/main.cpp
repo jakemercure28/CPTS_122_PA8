06a3e5e04561ada9431db0a722cf40526f62be99
Subashree Venkatasubramanian <s.venkatasubramani2@wsu.edu>
2020-12-03 17:04:32 -0800

    Added functions for the game menu implementation file, defined in pa8.h header file.

--- PinballSetUp/PinballSetUp/main.cpp
+++ PinballSetUp/PinballSetUp/main.cpp
@@ -65,30 +65,8 @@ int main()
 	}
 
 	sf::Text title; Text titleshadow; Text score; Text scoreshadow;
-	//title 
-	title.setFont(MyFont);
-	title.setString("PINBALL");
-	title.setCharacterSize(70);
-	title.setFillColor(sf::Color::Red);
-	title.setPosition(25.f, 750.f);
-	//shadow of title
-	titleshadow.setFont(MyFont);
-	titleshadow.setString("PINBALL");
-	titleshadow.setCharacterSize(70);
-	titleshadow.setFillColor(sf::Color::Blue);
-	titleshadow.setPosition(30.f, 755.f);
-	//score
-	score.setFont(MyFont);
-	score.setString("Score:");
-	score.setCharacterSize(50);
-	score.setFillColor(sf::Color::Red);
-	score.setPosition(45.f, 50.f);
-	//shadow of score
-	scoreshadow.setFont(MyFont);
-	scoreshadow.setString("Score:");
-	scoreshadow.setCharacterSize(50);
-	scoreshadow.setFillColor(sf::Color::Blue);
-	scoreshadow.setPosition(50.f, 55.f);
+	title_score_game_setup(&MyFont, &title, &titleshadow, &score, &scoreshadow);
+	
 
 	sf::RectangleShape score_box1(sf::Vector2f(170.f, 60.f));
 	score_box1.setPosition(55.f, 115.f);
@@ -111,56 +89,16 @@ int main()
 	//text boxes for menu
 	sf::Text manu_title; Text created_by; Text startplayable; Text testcase1; Text testcase2; Text testcase3; Text testcase4; Text testcase5; Text exitText;
 	//text setting
-	manu_title.setFont(MyFont); startplayable.setFont(MyFont); testcase1.setFont(MyFont);  testcase2.setFont(MyFont); testcase3.setFont(MyFont);
-	testcase4.setFont(MyFont); testcase5.setFont(MyFont); exitText.setFont(MyFont);
-	manu_title.setString("PINBALL");
-	manu_title.setCharacterSize(70);
-	manu_title.setFillColor(sf::Color::Red);
-	manu_title.setPosition(330.f, 100.f);
-
-
-	startplayable.setString("Start Playable Game");
-	startplayable.setCharacterSize(25);
-	startplayable.setFillColor(sf::Color::Red);
-	startplayable.setPosition(325.f, 260.f);
-
-	testcase1.setString("Test Case 1");
-	testcase1.setCharacterSize(25);
-	testcase1.setFillColor(sf::Color::Red);
-	testcase1.setPosition(395.f, 340.f);
-
-	testcase2.setString("Test Case 2");
-	testcase2.setCharacterSize(25);
-	testcase2.setFillColor(sf::Color::Red);
-	testcase2.setPosition(395.f, 420.f);
-
-	testcase3.setString("Test Case 3");
-	testcase3.setCharacterSize(25);
-	testcase3.setFillColor(sf::Color::Red);
-	testcase3.setPosition(395.f, 500.f);
-
-	testcase4.setString("Test Case 4");
-	testcase4.setCharacterSize(25);
-	testcase4.setFillColor(sf::Color::Red);
-	testcase4.setPosition(395.f, 580.f);
-
-	testcase5.setString("Test Case 5");
-	testcase5.setCharacterSize(25);
-	testcase5.setFillColor(sf::Color::Red);
-	testcase5.setPosition(395.f, 660.f);
-
-	exitText.setString("Exit Game");
-	exitText.setCharacterSize(25);
-	exitText.setFillColor(sf::Color::Red);
-	exitText.setPosition(410.f, 740.f);
+	
 
 	sf::RectangleShape mouse_check(sf::Vector2f(10.f, 10.f));
 
 	//menu initializations
 	create_menu(&outerbox, &start_playable, &test1, &test2, &test3, &test4, &test5, &exit);
-
+	create_menu2(&MyFont, &manu_title, &created_by, &startplayable, &testcase1, &testcase2, &testcase3, &testcase4, &testcase5, &exitText);
 
 	int user_choice = 0;
+	int count = 0;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -175,63 +113,21 @@ int main()
 		//first, display the menu before nay option is chosen
 		if (user_choice == 0) {
 			window.clear();
-			window.draw(outerbox);
-			window.draw(test1);
-			window.draw(start_playable);
-			window.draw(test2);
-			window.draw(test3);
-			window.draw(test4);
-			window.draw(test5);
-			window.draw(exit);
-			window.draw(manu_title);
-			window.draw(startplayable);
-			window.draw(testcase1);
-			window.draw(testcase2);
-			window.draw(testcase3);
-			window.draw(testcase4);
-			window.draw(testcase5);
-			window.draw(exitText);
+
+			draw_menu(&window, outerbox, test1, start_playable, test2, test3, test4, test5, exit);
+			draw_menu2(&window, manu_title, startplayable, testcase1, testcase2, testcase3, testcase4, testcase5, exitText);
+
 			window.display();
 		}
-
-		if (Mouse::isButtonPressed(sf::Mouse::Left))
-		{
-			Vector2i mouse_pos = Mouse::getPosition(window);
-
-
-			//1, start game option
-			if ((((mouse_pos.x > 300) && (mouse_pos.x < 650)) && ((mouse_pos.y > 250) && (mouse_pos.y < 300)))) {
-				user_choice = 1;
-			}
-			//2, test case 1 option
-			else if ((((mouse_pos.x > 300) && (mouse_pos.x < 650)) && ((mouse_pos.y > 330) && (mouse_pos.y < 380)))) {
-				user_choice = 2;
-
-			}
-			//3, test case 2 option
-			else if ((((mouse_pos.x > 300) && (mouse_pos.x < 650)) && ((mouse_pos.y > 410) && (mouse_pos.y < 460)))) {
-				user_choice = 3;
-			}
-			//4, test case 3 option
-			else if ((((mouse_pos.x > 300) && (mouse_pos.x < 650)) && ((mouse_pos.y > 490) && (mouse_pos.y < 540)))) {
-				user_choice = 4;
-			}
-			//5, test case 4 option
-			else if ((((mouse_pos.x > 300) && (mouse_pos.x < 650)) && ((mouse_pos.y > 570) && (mouse_pos.y < 620)))) {
-				user_choice = 5;
-			}
-
-			//6, test case 5 option
-			else if ((((mouse_pos.x > 300) && (mouse_pos.x < 650)) && ((mouse_pos.y > 650) && (mouse_pos.y < 700)))) {
-				user_choice = 6;
-			}
-
-			//7, exit option
-			else if ((((mouse_pos.x > 300) && (mouse_pos.x < 650)) && ((mouse_pos.y > 730) && (mouse_pos.y < 780)))) {
-				user_choice = 7;
+		if (count == 0) {
+			if (Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				Vector2i mouse_pos = Mouse::getPosition(window);
+				user_choice = check_mouse_pos(mouse_pos);
 			}
 		}
 		if ((user_choice == 1) || (user_choice == 2) || (user_choice == 3) || (user_choice == 4) || (user_choice == 5) || (user_choice == 6)) {
+			count++;
 			if (Keyboard::isKeyPressed(Keyboard::A))
 			{
 				//Rotate Left Flipper CCW until max rotation
@@ -280,7 +176,9 @@ int main()
 			window.draw(score_box2);
 			window.draw(score_box1);
 			window.display();
+			//count = 1;
 		}
+
 		if (user_choice == 7) {
 			window.close();
 		}
