cc474eb48ed94168bcf435c36a608feaafa4e35c
Subashree Venkatasubramanian <s.venkatasubramani2@wsu.edu>
2020-12-06 19:31:55 -0800

    Ball class comments

--- PinballSetUp/PinballSetUp/ball.cpp
+++ PinballSetUp/PinballSetUp/ball.cpp
@@ -1,5 +1,11 @@
 #include "ball.h"
 #include "sound.h"
+
+
+//Description: This is the .cpp (source) file for the ball class, which contains the definitions for
+//the functionalities used to create the pinball in our pinball game.
+
+//the constructor for the ball object
 ball::ball(float initX, float initY)
 {
 	position.x = initX;
@@ -9,58 +15,67 @@ ball::ball(float initX, float initY)
 	ballShape.setPosition(position);
 }
 
+//returns the position vector
 Vector2f ball::getposition()
 {
 	return position;
 }
 
+//returns the ball object
 CircleShape ball::getShape()
 {
 	return ballShape;
 }
 
+//returns the origin/center point of the ball
 Vector2f ball::getOrigin()
 {
 	return Vector2f(ballShape.getPosition().x + ballShape.getRadius(),
 		ballShape.getPosition().y + ballShape.getRadius());
 }
 
+//applies gravity to the ball's movement
 void ball::ApplyGravity(Vector2f gravity)
 {
 	Velocity.x += gravity.x;
 	Velocity.y += gravity.y;
 }
 
+//returns the ball's velocity
 Vector2f ball::getVelocity()
 {
 	return Velocity;
 }
 
+//this function simulates the ball's collision against a ConvexShape object
+//returns true if collision happened, false if not
 bool ball::collision(ConvexShape shape)
 {
 	Vector2f originA = getOrigin();
 
 	// Circle to Vertex Collision
-	for (int i = 0; i < shape.getPointCount();i++)
+	for (int i = 0; i < shape.getPointCount(); i++)
 	{
 		Vector2f rotatedPoint = rotatePoint(shape.getPoint(i), shape.getRotation());
 		float xDiff = (originA.x - (rotatedPoint.x + shape.getPosition().x));
 		float yDiff = (originA.y - (rotatedPoint.y + shape.getPosition().y));
 
 		float distance = sqrt(xDiff * xDiff + yDiff * yDiff);
+		//if the distance between the ball and the shape is less than or equal to the radius, 
+		//the ball is deflected off of it
 		if (distance <= ballShape.getRadius())
 		{
 			Vector2f normal = Vector2f(xDiff / distance, yDiff / distance);
 			float ddotn = normal.x * Velocity.x + normal.y * Velocity.y;
 			Vector2f int1 = (2 * ddotn) * normal;
 			Velocity = REBOUND * (Velocity - int1);
- 			return true; 
+			return true; //true means that the collision did occur, this is used to increment the score
 		}
 	}
 
 	int j;
 	// Circle to Line Collision
-	for (int i = 0; i < shape.getPointCount();i++)
+	for (int i = 0; i < shape.getPointCount(); i++)
 	{
 
 		if (i == 0)
@@ -74,26 +89,30 @@ bool ball::collision(ConvexShape shape)
 		float length = distance(linePt1, linePt2);
 		float dotp = (((originA.x - linePt1.x) * (linePt2.x - linePt1.x)) +
 			((originA.y - linePt1.y) * (linePt2.y - linePt1.y))) / (length * length);
-
+		//find the point closest to the ball, to see if the collision should happen
 		Vector2f closestPoint = Vector2f((linePt1.x + (dotp * (linePt2.x - linePt1.x))), (linePt1.y + (dotp * (linePt2.y - linePt1.y))));
 		if (!linePoint(linePt1, linePt2, closestPoint))
 		{
 			continue;
 		}
 		float trueDistance = distance(originA, closestPoint);
+		//if the distance between the ball and the shape is less than or equal to the radius, 
+		//the ball is deflected off of it
 		if (trueDistance <= ballShape.getRadius())
 		{
 			Vector2f normal = Vector2f((originA.x - closestPoint.x) / trueDistance, (originA.y - closestPoint.y) / trueDistance);
 			float ddotn = normal.x * Velocity.x + normal.y * Velocity.y;
 			Vector2f int1 = (2 * ddotn) * normal;
 			Velocity = REBOUND * (Velocity - int1);
-			return true;
+			return true;//true means that the collision did occur, this is then used to increment score
 		}
 	}
 	return false;
 
 }
 
+//this function simulates the ball's collision against a circlular object
+//returns true if collision happened, false if not
 bool ball::collision(CircleShape shape)
 {
 	Vector2f originA = getOrigin();
@@ -105,7 +124,9 @@ bool ball::collision(CircleShape shape)
 	float ySqr = yDiff * yDiff;
 
 	float distance = sqrt(xSqr + ySqr);
-	
+
+	//if the distance between the circle shape and the ball is less than/equal to the sum of their
+	//radii, the collision ocurs
 	if (distance <= shape.getRadius() + ballShape.getRadius())
 	{
 		// r = d - 2(d*n)n
@@ -115,34 +136,23 @@ bool ball::collision(CircleShape shape)
 		Vector2f normal = Vector2f(xDiff / distance, yDiff / distance);
 
 		float ddotn = normal.x * Velocity.x + normal.y * Velocity.y;
-		Vector2f  int1 = (2 * ddotn) * normal; 
+		Vector2f  int1 = (2 * ddotn) * normal;
 		Velocity = REBOUND * (Velocity - int1);
-		return true;
+		return true;//true means that the collision did occur, this is then used to increment score
 	}
 	return false;
 }
 
+//this function allows the ball to 'bounce' off of the boundaries
 void ball::hitboundary(FloatRect bound)
 {
 	if (position.x <= bound.left || position.x >= (bound.left + bound.width))
-		Velocity.x *= -REBOUND;
+		Velocity.x *= -REBOUND; //reverses the x direction of the ball
 	if (position.y <= bound.left || position.y >= (bound.top + bound.height))
-		Velocity.y *= -REBOUND;
+		Velocity.y *= -REBOUND;//reverses the y direction of the ball
 }
 
-void ball::hitboundary2(int width, int height)
-{
-	if ((((position.x) < (800)) || ((position.x) > (820))) && ((position.y) > (100)))
-	{
-		if ((position.x) >= (width - 2 * ballShape.getRadius()) || position.x <= 0)
-			Velocity.x *= -1.;
-
-		if (position.y >= (height - 2 * ballShape.getRadius()) || position.y <= 0)
-			Velocity.y *= -1.;
-	}
-}
-
-
+//updates the ball's position from its velocity
 void ball::update()
 {
 	ApplyGravity(Vector2f(0.f, 0.0025f));
@@ -150,27 +160,8 @@ void ball::update()
 	ballShape.setPosition(position);
 }
 
-void ball::launch(int setting)
-{
-	Velocity.x = 0;
-	Velocity.y = 0;
 
-	float gravity = 0.01f;
-	if (setting == 1) {
-		Velocity.y = -1;
-	}
-	else if (setting == 2) {
-		Velocity.y = -5;
-	}
-	else if (setting == 3) {
-		Velocity.y = -20;
-	}
-
-	ApplyGravity(Vector2f(0.f, gravity));
-	position = position + getVelocity();
-	ballShape.setPosition(position);
-}
-
+//rotates a point based on the inputted angle, and returns rotated point
 Vector2f ball::rotatePoint(Vector2f point, float angle)
 {
 
@@ -181,6 +172,7 @@ Vector2f ball::rotatePoint(Vector2f point, float angle)
 	return rotatedPoint;
 }
 
+//checks if a point if present on the given line
 bool ball::linePoint(Vector2f lineP1, Vector2f lineP2, Vector2f point)
 {
 	float dist1 = distance(point, lineP1);
@@ -194,6 +186,7 @@ bool ball::linePoint(Vector2f lineP1, Vector2f lineP2, Vector2f point)
 		return false;
 }
 
+//calculates the distance between two inputted points, and returns the float result
 float ball::distance(Vector2f point1, Vector2f point2)
 {
 	float xDiff = point1.x - point2.x;
@@ -203,6 +196,7 @@ float ball::distance(Vector2f point1, Vector2f point2)
 	return distance;
 }
 
+//mutator function, sets the velocity vector of the ball with the inputted x and y values
 void ball::setVelocity(float xvel, float yvel)
 {
 	Velocity.x = xvel;
--- PinballSetUp/PinballSetUp/ball.h
+++ PinballSetUp/PinballSetUp/ball.h
@@ -8,31 +8,63 @@
 #include <iostream>
 
 using namespace sf;
+
+//Description: This is the header file for the ball class, which contains the functionalities
+//used to create the pinball in our pinball game.
+
 class ball
 {
 private:
+	//variables to represent the ball's position, shape, and velocity
 	Vector2f position;
 	CircleShape ballShape;
 	Vector2f Velocity;
 
 public:
-
+	//the constructor for the ball object
 	ball(float initX, float initY);
+
+	//returns the position of the ball
 	Vector2f getposition();
+
+	//returns the ball object shape
 	CircleShape getShape();
+
+	//applies gravity to the ball's movement
 	void ApplyGravity(Vector2f gravity);
+
+	//returns the velocity of the ball
 	Vector2f getVelocity();
+
+	//returns the center point of the ball object
 	Vector2f getOrigin();
+
+	//this function allows the ball to 'bounce' off of the boundaries
 	void hitboundary(FloatRect bound);
+
+	//this function updates the ball's position
 	void update();
+
+	//this function simulates the ball's collision against a circlular object
+	//returns true if collision happened, false if not
 	bool collision(CircleShape shape);
+
+	//this function simulates the ball's collision against a ConvexShape object
+	//returns true if collision happened, false if not
 	bool collision(ConvexShape shape);
-	void launch(int setting);
+
+	//rotate function to simulate ball rotation
 	Vector2f rotatePoint(Vector2f point, float angle);
+
+	//this function returns the distance between 2 points
 	float distance(Vector2f point1, Vector2f point2);
+
+	//this function is used to calculate the collisions
 	bool linePoint(Vector2f lineP1, Vector2f lineP2, Vector2f point);
+
+	//this function accepts 2 float variables which it sets as the ball's velocity
 	void setVelocity(float xvel, float yvel);
-	void hitboundary2(int width, int height);
+
 };
 
 #endif
